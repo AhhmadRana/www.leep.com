0da22da3016c4981d291b95eab79ae93ec31fe84
Muhammad Ahmad <ahmed.nazreen97@gmail.com>
2024-12-29 18:58:36 +0500

    first commit

--- client/src/pages/Campaign.jsx
+++ client/src/pages/Campaign.jsx
@@ -12,6 +12,8 @@ import axios from 'axios'
 import { logo } from '../assets'
 import vrPerson from '../assets/vr.png'
 
+//import abovethefold from "../assets/technology.jpg"
+
 import { ProjectsContext } from "../contexts/ProjectsProvider";
 import LoggeInRequired from "../components/logginRequired";
 
@@ -55,7 +57,7 @@ const Campaign = () => {
   };
 
   const handleSave = () => {
-    axios.put(`http://localhost:5200/api/projects/${compaignProject.id}`, compaignProject)
+    axios.put('http://localhost:5200/api/projects/${compaignProject.id}', compaignProject)
       .then(response => {
         console.log(response.data);
       })
@@ -83,6 +85,7 @@ const Campaign = () => {
     <div>
       <div className="relative isolate overflow-hidden bg-gray-900 py-24 sm:py-32">
         <img
+        //src={abovethefold}
           src="https://images.unsplash.com/photo-1521737604893-d14cc237f11d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&crop=focalpoint&fp-y=.8&w=2830&h=1500&q=80&blend=111827&sat=-100&exp=15&blend-mode=multiply"
           alt=""
           className="absolute inset-0 -z-10 h-full w-full object-cover object-right md:object-center"
